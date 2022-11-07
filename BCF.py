e86b0d83e7b2fdea79edd461c3c14807a1bbce1e
Deraa Leviathanzky <117666570+SiestaaXD@users.noreply.github.com>
2022-11-07 23:10:30 +0700

    Create BCF.py

---
+++ BCF.py
@@ -0,0 +1,11 @@
+$pkg update && pkg upgrade 
+$pkg install python
+$pkg install git 
+$pip install requests 
+$pip install mechanize 
+$pip install bs4
+$rm -rf BCF
+$git clone https://github.com/P4HRUL/BCF
+$cd BCF
+$git pull
+$python BCF.py
